6dee3011892923b44c14d066c4e4193dde2629ff
Chris Konnertz <siconize@gmail.com>
2018-01-07 19:07:47 +0100

    Added code so we have a minimalistic prototype

--- src/ChrisKonnertz/RegExMaker/RegExMaker.php
+++ src/ChrisKonnertz/RegExMaker/RegExMaker.php
@@ -9,5 +9,50 @@ class RegExMaker
      * The current version number
      */
     const VERSION = '0.0.1';
+    
+    protected $expressions = [];
+    
+    public function __construct()
+    {
+        // TODO remove empty constructor?
+    }
+    
+    public function addAnd($value)
+    {
+        if ($value isntanceof \Closure) {
+            $value = $value($this);
+            
+            // TODO validate $value
+        }
+        
+        $andEx = new Expressions\AndEx($value);
+        $this->expressions[] = $andEx;
+    }
+    
+    /**
+     * Returns the complete regular expressions as a string
+     * 
+     * @return string
+     */
+    public function getRegEx()
+    {        
+        $regEx = '';
+        
+        foreach ($this->expression as $expression) {
+            $regEx .= $expression->getRegEx();
+        }
+        
+        return $regEx;
+    }
+    
+    /**
+     * This PHP magic method returns the regular expression as a string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getRegEx();
+    }       
 
 }
