d6756d4a8102ebf31f966ca2cd0331fb0c1880a7
Chris Konnertz <siconize@gmail.com>
2019-02-15 13:08:52 +0100

    Adjusted version number in RegEx class

--- src/ChrisKonnertz/RegEx/RegEx.php
+++ src/ChrisKonnertz/RegEx/RegEx.php
@@ -57,7 +57,7 @@ class RegEx
     /**
      * The current version number
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * The start of the regular expression (=prefix)
